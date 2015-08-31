f6c9bf4174c7c1b6226cf06a1d65d671b95c5f16
Sam Ottenhoff <ottenhoff@longsight.com>
2015-08-31 18:56:50 -0400

    First commit

---
+++ ckeditor/kaltura/plugin.js
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2014 Sakaiproject Licensed under the
+ * Educational Community License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+//var CKEDITOR = CKEDITOR || {};
+var ckeditorId;
+(function() {
+    var pluginName = 'kaltura';
+    CKEDITOR.plugins.add(pluginName,
+        {
+            init: function(editor) {
+
+                ckeditorId = editor.name;
+                var height = 480;
+                var width = 770;
+                CKEDITOR.dialog.addIframe(pluginName,
+                    'Kaltura',
+                    '/media-gallery-tool/ckeditor.htm?userid=' + parent.portal.user.id + '&siteid=' + parent.portal.siteId,
+                    width,
+                    height,
+                    function() {
+                        var ckDialog = CKEDITOR.dialog.getCurrent();
+                        document.getElementById(ckDialog.getButton('ok').domId).style.display='none';
+                        document.getElementById(ckDialog.getButton('cancel').domId).style.display='none';
+                    },
+
+                    {
+                        onOk : function() {}
+                    }
+                );
+                editor.addCommand(pluginName, new CKEDITOR.dialogCommand( 'kaltura' ) );
+
+                editor.ui.addButton(pluginName, {
+                    label: pluginName,
+                    command: pluginName,
+                    icon: this.path + 'images/kaltura.gif'
+                });
+            }
+        }
+    );
+
+})();
---
+++ impl/src/main/java/org/sakaiproject/kaltura/impl/dao/KalturaLtiRoleDaoImpl.java
@@ -0,0 +1,193 @@
+/**
+ * Copyright 2014 Sakaiproject Licensed under the
+ * Educational Community License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package org.sakaiproject.kaltura.impl.dao;
+
+import java.util.Date;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.sakaiproject.genericdao.api.search.Search;
+import org.sakaiproject.genericdao.hibernate.HibernateGeneralGenericDao;
+import org.sakaiproject.kaltura.api.dao.KalturaLtiRoleDao;
+import org.sakaiproject.kaltura.models.dao.KalturaLtiRole;
+
+/**
+ * Implementation of DAO Interface for Kaltura custom role mappings
+ * 
+ * @author Robert Long (rlong @ unicon.net)
+ */
+public class KalturaLtiRoleDaoImpl extends HibernateGeneralGenericDao implements KalturaLtiRoleDao {
+
+    private final Log log = LogFactory.getLog(KalturaLtiRoleDaoImpl.class);
+
+    public void init() {
+    }
+
+    public void destroy(){
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<KalturaLtiRole> getAllRoleMappings() {
+        List<KalturaLtiRole> allRoleMappings = findAll(KalturaLtiRole.class);
+
+        return allRoleMappings;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public KalturaLtiRole getRoleMapping(long roleMappingId) {
+        Search search = new Search("id", roleMappingId);
+
+        KalturaLtiRole kalturaLtiRole = findOneBySearch(KalturaLtiRole.class, search);
+
+        return kalturaLtiRole;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public KalturaLtiRole getRoleMapping(String roleMappingId) {
+        long id = Long.parseLong(roleMappingId);
+
+        return getRoleMapping(id);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<KalturaLtiRole> getSakaiRoleMappings(String sakaiRole) {
+        Search search = new Search("sakaiRole", sakaiRole);
+
+        List<KalturaLtiRole> kalturaLtiRoles = findBySearch(KalturaLtiRole.class, search);
+
+        return kalturaLtiRoles;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<KalturaLtiRole> getLtiRoleMappings(String ltiRole) {
+        Search search = new Search("ltiRole", ltiRole);
+
+        List<KalturaLtiRole> ltiRoleMappings = findBySearch(KalturaLtiRole.class, search);
+
+        return ltiRoleMappings;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public KalturaLtiRole getRoleMapping(String sakaiRole, String ltiRole) {
+        String[] properties = new String[] {"sakaiRole", "ltiRole"};
+        String[] values = new String[] {sakaiRole, ltiRole};
+        Search search = new Search(properties, values);
+
+        KalturaLtiRole kalturaLtiRole = findOneBySearch(KalturaLtiRole.class, search);
+
+        return kalturaLtiRole;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void save(KalturaLtiRole kalturaLtiRole) {
+        if (!kalturaLtiRole.isValid()) {
+            kalturaLtiRole = new KalturaLtiRole(kalturaLtiRole);
+        }
+
+        kalturaLtiRole.setDateModified(new Date());
+
+        commit(kalturaLtiRole, false);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void delete(KalturaLtiRole kalturaLtiRole) {
+        delete(kalturaLtiRole.getId());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void delete(String id) {
+        long longId = Long.parseLong(id);
+
+        delete(longId);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void delete(long id) {
+        KalturaLtiRole kalturaLtiRole = getRoleMapping(id);
+
+        if (kalturaLtiRole != null) {
+            commit(kalturaLtiRole, true);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void commit(KalturaLtiRole kalturaLtiRole, boolean delete) {
+        getHibernateTemplate().flush();
+
+        Session session = getSessionFactory().openSession();
+        Transaction transaction = session.getTransaction();
+
+        try {
+            transaction = session.beginTransaction();
+
+            if (delete) {
+                session.delete(kalturaLtiRole);
+            } else {
+                session.saveOrUpdate(kalturaLtiRole);
+            }
+
+            transaction.commit();
+        } catch ( Exception e) {
+            if (delete) {
+                log.error("Kaltura :: deleteRoleMapping : An error occurred deleting the role mapping: " + kalturaLtiRole.toString() + ", error: " + e, e);
+            } else {
+                log.error("Kaltura :: addRoleMapping : An error occurred persisting the role mapping: " + kalturaLtiRole.toString() + ", error: " + e, e);
+            }
+
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        } finally {
+            session.close();
+        }
+    }
+
+}
